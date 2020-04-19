554482ceadd186f260704809086bb58c2712412e
gagan-soni <63969564+gagan-soni@users.noreply.github.com>
2020-04-19 22:32:33 +0530

    Create NumDataType.py

---
+++ NumDataType.py
@@ -0,0 +1,35 @@
+#Number Data Type in Python
+"""
+This file is created
+to show the way various number data types
+in the python
+"""
+
+"""
+Following are the number data types available in python
+1. Integer (Example Num = 10)
+2. Long - (Example Num = 875276987979345)
+3. Float - (Example Num = 5.4)
+4. Complex - (Example Num = 3+5j)
+"""
+
+#1. Integer number
+intNum = 20 # Creating an integer Variable and assigning a value to this variable
+print("Integer Number:")
+print(intNum) #Printing the Integer number
+
+
+#2. Long number
+longNum = 879876868686 # Creating a Long Variable and assigning a value to this variable
+print("Long Number:")
+print(longNum) #Printing the Long number
+
+#3. Float number
+floatNum = 16.89 # Creating a Float Variable and assigning a value to this variable
+print("Float Number:")
+print(floatNum) #Printing the Float number
+
+#4. Complex number
+imgNum = 4+7j # Creating an Imaginary Variable and assigning a value to this variable
+print("Complex Number:")
+print(imgNum) # Printing the Imaginary number
